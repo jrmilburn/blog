f531b0160d81572141968355b998eb9a8484e3db
Joe Milburn <jrmilburn@outlook.com>
2024-08-24 21:34:28 +1000

    update cors headers

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend's URL
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use(session({
     secret: process.env.SECRET,
